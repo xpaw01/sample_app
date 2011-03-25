1c9f84d7345a22c74bd7dc0694dfd49c9f211539
Paul Burke <paul@ticketderby.com>
2011-03-25 00:32:47 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3f8aedec808b97e8837ed89fe1202198a20a696a12fd003c96b183f1792b32c08bc092e850467d3350e49ba2c724bc8fea3fdb8f97d7a6ceea3e5d60ffb72d60'
